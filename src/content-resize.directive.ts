302a8d8dc6871e2b31c4d4ba24f62a1dbf01df3f
Ruben Soler <r.solerginer@gmail.com>
2018-02-07 18:22:28 +0100

    refactor(content-resize.directive.ts): better structure

--- src/content-resize.directive.ts
+++ src/content-resize.directive.ts
@@ -16,24 +16,34 @@ export class ContentResizeDirective implements OnInit {
   @Input() bottomBarHeight: any;
 
   private _parentContainerHeight: number;
+  private _componentSectionsHeight: number;
 
   constructor(private el: ElementRef) { }
 
   ngOnInit(): void {
+    this._componentSectionsHeight = (this.topBarHeight + this.bottomBarHeight) - 3;
     if (this.isFullScreen) {
-      this.el.nativeElement.style.height = (window.innerHeight - (this.topBarHeight + this.bottomBarHeight)).toString() + 'px';
+      this.setFullScreenHeight();
     } else {
       this._parentContainerHeight = this.el.nativeElement.parentNode.parentNode.parentNode.height;
-      this.el.nativeElement.style.height = (this._parentContainerHeight - (this.topBarHeight + this.bottomBarHeight)).toString() + 'px';
+      this.setParentHeight();
     }
   }
 
   @HostListener('window:resize', ['$event']) onWindowResize(event: any) {
     if (this.isFullScreen) {
-      this.el.nativeElement.style.height = (window.innerHeight - (this.topBarHeight + this.bottomBarHeight)).toString() + 'px';
+      this.setFullScreenHeight();
     } else {
-      this.el.nativeElement.style.height = (this._parentContainerHeight - (this.topBarHeight + this.bottomBarHeight)).toString() + 'px';
+      this.setParentHeight();
     }
+  }
+
+  setFullScreenHeight() {
+    this.el.nativeElement.style.height = (window.innerHeight - this._componentSectionsHeight).toString() + 'px';
+  }
+
+  setParentHeight() {
+    this.el.nativeElement.style.height = (this._parentContainerHeight - this._componentSectionsHeight).toString() + 'px';
   }
 
 }
